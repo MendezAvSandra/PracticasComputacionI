f4f29f7a052c1e70c0df3b137246f48f344fad92
MendezAvSandra <89266144+MendezAvSandra@users.noreply.github.com>
2021-10-15 11:50:15 -0500

    Update Punto.cpp

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -3,14 +3,10 @@
  * mendezavilessandra@gmail.com
  * Oct 15, 2021
  */
-#include <iostream>
-#include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 #define PI 3.1415926536
-using namespace std;
 #include "Punto.h"
-
+using namespace std;
 void Punto2D::SetPosicion(double a, double b) {
     x=a;
     y=b;
@@ -19,23 +15,27 @@ void Punto2D::SetPosicion(double a, double b) {
 float Punto2D::Escalar(float temp1, float temp2) {
     x= x*temp1;
     y= y*temp2;
+    return 0;
 }
 
-double Punto2D::RotarRespectoAlOrigen(float temp1) {
-    temp1= (temp1*PI)/180;
+float Punto2D::RotarRespectoAlOrigen(float temp1) {
+    temp1= temp1 * PI / 180;
     float xe=x;
     x= (x*cos(temp1))-(y*sin(temp1));
     y= (xe*sin(temp1))+(y*cos(temp1));
+    return 0;
 }
 
 
 float Punto2D::Trasladar(float temp1, float temp2) {
     x=x+temp1;
-    y=y+temp2; }
+    y=y+temp2;
+    return 0;
+}
 
-float Punto2D::GetX() {
+float Punto2D::GetX() const {
     return x;
 }
-float Punto2D::GetY() {
+float Punto2D::GetY() const {
     return y;
 }
