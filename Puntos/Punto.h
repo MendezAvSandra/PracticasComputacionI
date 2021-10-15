7062b59752d44450edc043a81bdaa2c1c2b2f369
MendezAvSandra <89266144+MendezAvSandra@users.noreply.github.com>
2021-10-15 00:38:42 -0500

    Update Punto.h

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -1,1 +1,18 @@
-/* Author: Méndez Avilés Sandra
+/*
+ * Author: Sandra Mendez Aviles
+ * mendezavilessandra@gmail.com
+ * Oct 15, 2021
+ */
+
+#ifndef DDD_PUNTO_H
+#define DDD_PUNTO_H
+class Punto2D{
+public:
+    float x, y;
+    void SetPosicion(double a, double b);
+    float Escalar(float temp1, float temp2);
+    float GetX(), GetY();
+    double RotarRespectoAlOrigen(float temp1);
+    float Trasladar (float temp1, float temp2);
+};
+#endif //DDD_PUNTO_H
